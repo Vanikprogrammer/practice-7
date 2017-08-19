a9714850afdc8d0567e9404078c8266df1416c33
Ivan <ngk-trade@mail.ru>
2017-08-19 14:35:28 +0300

    Все работает

---
+++ src/FixedPayment.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by 1 on 12.08.2017.
+ */
+public class FixedPayment {
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by 1 on 19.08.2017.
+ */
+public class Main {
+}
